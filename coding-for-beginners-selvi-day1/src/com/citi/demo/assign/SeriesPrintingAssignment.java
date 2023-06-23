17866b059264153763e666667fda5fbb263a302e
SelviNachimuthu <maheswaranselvi@gmail.com>
2023-06-23 11:00:36 +0000

    Project to display

--- coding-for-beginners-selvi-day1/src/com/citi/demo/assign/SeriesPrintingAssignment.java
+++ coding-for-beginners-selvi-day1/src/com/citi/demo/assign/SeriesPrintingAssignment.java
@@ -3,7 +3,7 @@ package com.citi.demo.assign;
 public class SeriesPrintingAssignment {
 
 	public static void main(String[] args) {
-	 
+	  System.out.println("Project to print Number Series");
 		for (int i = 1;i<6;i++) {
 			for (int j = 1; j<=i;j++) {
 			//	System.out.print(i);
